369203eaed44ee3433c5554469df67b6a73c0430
suga-ming <kby9901@naver.com>
2022-10-12 15:22:52 +0900

    로딩 스피너

--- src/component/project 2/PJ_2.js
+++ src/component/project 2/PJ_2.js
@@ -3,9 +3,11 @@ import "./PJ_2.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherBox from "./WeatherBox";
 import WeatherButton from "./WeatherButton";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const PJ2 = () => {
   const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [city, setCity] = useState("");
   const cities = ["paris", "new york", "tokyo", "seoul"];
 
@@ -19,16 +21,20 @@ const PJ2 = () => {
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=de49f1b27238e4df2be879a8764c4f57&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
+    setLoading(false);
   };
 
   const getWeatherByCity = async () => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=de49f1b27238e4df2be879a8764c4f57&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -41,10 +47,16 @@ const PJ2 = () => {
 
   return (
     <div>
-      <div className="weather-box">
-        <WeatherBox weather={weather} />
-        <WeatherButton cities={cities} setCity={setCity} />
-      </div>
+      {loading ? (
+        <div className="weather-box">
+          <ClipLoader color="#f88c6b" loading={loading} size={150} />
+        </div>
+      ) : (
+        <div className="weather-box">
+          <WeatherBox weather={weather} />
+          <WeatherButton cities={cities} setCity={setCity} />
+        </div>
+      )}
     </div>
   );
 };
